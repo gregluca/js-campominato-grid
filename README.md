d45fe87452b0c1a38debb528ffaa33e1fb2d471b
Luca Gregori <luca.gregori98@gmail.com>
2023-10-30 14:03:35 +0100

    Add: read me - grid

---
+++ README.md
@@ -0,0 +1,8 @@
+### Esercizio 
+#### Campominato
+
+* creare griglia alla funzione del click sul bottone 
+* creare un ciclo for da 1 a 100 
+* creare una cella per ogni numero del ciclo 
+* al click di ogni cella inserire il colore azzurro 
+e stampare il numero della cella in console 
--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
+// const gridElem = document.querySelector(".grid");
+
+
+// for (let i = 1; i <=100; i++) {
+//   const cell = document.createElement("div");
+//   cell.classList.add("cell");
+//   cell.textContent = i;
+//   gridElem.appendChild(cell).addEventListener("click",change);
+//   cell.addEventListener("click", function() {
+//     console.log(i);
+//   })
+// }
+// console.log(gridElem);
+
+// function change (color) {
+//   this.classList.toggle("orange");
+// }
+document.getElementById("btn").addEventListener("click", start);
 const gridElem = document.querySelector(".grid");
 
-for (let i = 1; i <=100; i++) {
-  const cell = document.createElement("div");
-  cell.classList.add("cell");
-  cell.textContent = i;
-  gridElem.appendChild(cell).addEventListener("click",change);
-  cell.addEventListener("click", function() {
-    console.log(i);
-  })
+function start () {
+  for (let i = 1; i <= 100; i++){
+    const newCell = document.createElement("div");
+    newCell.addEventListener("click", changecolor);
+    newCell.classList.add("cell");
+    newCell.innerHTML = i;
+    console.log(newCell);
+    gridElem.append(newCell);
+  }
 }
-console.log(gridElem);
 
-function change (color) {
-  this.classList.toggle("orange");
+function changecolor () {
+  this.classList.add("orange");
+  console.log(this.textContent);
 }
